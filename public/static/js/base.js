c6ebdbf5e6201aba694c973f0f8a926663011b59
madprops <manuelchaves@gmail.com>
2018-11-20 21:37:38 -0600

    Soundcloud Radio Fix - More

--- public/static/js/base.js
+++ public/static/js/base.js
@@ -16779,7 +16779,7 @@ Hue.start_soundcloud = function()
 		{
 			Hue.soundcloud_player = _soundcloud_player
 
-			if((Hue.last_radio_type && Hue.last_radio_type === "youtube") || Hue.current_radio().type === "soundcloud")
+			if((Hue.last_radio_type && Hue.last_radio_type === "soundcloud") || Hue.current_radio().type === "soundcloud")
 			{
 				Hue.change({type:"radio", notify:false})
 			}
