7feddf5b0e262478939cd387a7e59f51774b69f8
madprops/mc <manuelchaves@gmail.com>
2019-02-17 13:54:16 -0600

    Electron fix -

--- electron/preload.js
+++ electron/preload.js
@@ -1,21 +1,15 @@
-const remote = require('electron').remote
 const electron_notifier = require('node-notifier')
 
 window.electron_api = {}
 
 window.electron_api.highlighted = function(data)
 {
-	if(remote.getCurrentWindow().isFocused())
+	if(!document.hidden)
 	{
 		return false
 	}
 
 	electron_notify("Hue", "You were highlighted")
-}
-
-window.electron_api.close_window = function(data)
-{
-	remote.getCurrentWindow().close()
 }
 
 function electron_notify(title, msg)
