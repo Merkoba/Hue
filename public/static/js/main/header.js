361a2bc8c92b9e28f2e509f2bb3e6e488e4951cf
madprops/mc <manuelchaves@gmail.com>
2019-05-11 04:50:04 -0600

    Toy

--- public/static/js/main/header.js
+++ public/static/js/main/header.js
@@ -8,4 +8,16 @@ Hue.update_header_separators = function()
 Hue.setup_header = function()
 {
     Hue.horizontal_separator.separate("header_radio")
+
+    $("#room_menu_icon").on("auxclick", function(e)
+    {
+        if(e.which === 2)
+        {
+            let rotated = $("#main_container").data("hue_rotated")
+            let degrees = rotated ? 0 : 180
+            
+            $("#main_container").css("transform", `rotateY(${degrees}deg)`)
+            $("#main_container").data("hue_rotated", !Boolean(rotated))
+        }
+    })
 }
