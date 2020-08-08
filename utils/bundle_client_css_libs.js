bdd3e1607ba73466453c1949c104b8b30d1afb6b
Auric Vente <easywest@protonmail.com>
2020-08-08 03:30:23 -0600

    Package bumps

--- utils/bundle_client_css_libs.js
+++ utils/bundle_client_css_libs.js
@@ -3,16 +3,14 @@ const fs = require("fs")
 let dir_path = "../public/static/css/libs/"
 let dir_path_target = "../public/static/css/build/"
 let files = fs.readdirSync(dir_path)
-let content = ""
+let bundle = ""
 
-for(let file of files)
-{
-    if(!file.endsWith(".css") || file === "libs.bundle.min.css")
-    {
+for (let file of files) {
+    if (!file.endsWith(".css") || file === "libs.bundle.min.css") {
         continue
     }
 
-    content += fs.readFileSync(`${dir_path}${file}`, "utf8") + "\n"
+    bundle += fs.readFileSync(`${dir_path}${file}`, "utf8") + "\n"
 }
 
-fs.writeFileSync(`${dir_path_target}libs.bundle.min.css`, content, "utf8")
+fs.writeFileSync(`${dir_path_target}libs.bundle.min.css`, bundle, "utf8")
--- utils/bundle_client_js_libs.js
+++ utils/bundle_client_js_libs.js
@@ -1,27 +1,21 @@
 const fs = require("fs")
-const Terser = require("terser")
 
 let dir_path = "../public/static/js/libs/"
 let dir_path_target = "../public/static/js/build/"
-let minified = ""
+let bundle = ""
 let manual = ["jquery.min.js"]
 let files = fs.readdirSync(dir_path)
 
-for(let file of manual)
-{
-    let content = fs.readFileSync(`${dir_path}${file}`, "utf8")
-    minified += Terser.minify(content).code
+for (let file of manual) {
+    bundle += fs.readFileSync(`${dir_path}${file}`, "utf8") + "\n"
 }
 
-for(let file of files)
-{
-    if(manual.includes(file) || !file.endsWith(".js") || file === "libs.bundle.min.js")
-    {
+for (let file of files) {
+    if (manual.includes(file) || !file.endsWith(".js") || file === "libs.bundle.min.js") {
         continue
     }
 
-    let content = fs.readFileSync(`${dir_path}${file}`, "utf8")
-    minified += Terser.minify(content).code + "\n"
+    bundle += fs.readFileSync(`${dir_path}${file}`, "utf8") + "\n"
 }
 
-fs.writeFileSync(`${dir_path_target}libs.bundle.min.js`, minified, "utf8")
+fs.writeFileSync(`${dir_path_target}libs.bundle.min.js`, bundle, "utf8")
