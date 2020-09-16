7ac32afd5a487d693c46f3d8f913253e9ef1ad68
Auric Vente <easywest@protonmail.com>
2020-09-15 19:25:06 -0600

    infotip mod

--- public/static/js/main/infotip.js
+++ public/static/js/main/infotip.js
@@ -2,11 +2,13 @@
 // This is a black box in the corner meant for quick temporary feedback
 Hue.show_infotip = function (s) {
   $("#infotip").text(s)
-  $("#infotip_container").css("display", "block")
+  $("#infotip_container").css("display", "flex")
+  $("#activity_bar_inner").css("display", "none")
   Hue.infotip_timer()
 }
 
 // Hides the infotip
 Hue.hide_infotip = function () {
   $("#infotip_container").css("display", "none")
+  $("#activity_bar_inner").css("display", "grid")
 }
