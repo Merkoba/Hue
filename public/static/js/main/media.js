ffcf13abb840c9047795c05ab676f15ff049d24e
Auric Vente <mprops@protonmail.com>
2021-04-10 09:49:06 -0600

    load mod

--- public/static/js/main/media.js
+++ public/static/js/main/media.js
@@ -821,7 +821,8 @@ Hue.media_string = function (what) {
 // Load or restart media
 Hue.load_media = function (type, data) {
   Hue.toggle_media({type:type, what:true})
-    
+  Hue.change_media_lock({type:"tv", what:true})
+  
   Hue.change({
     type: type,
     item: data,
