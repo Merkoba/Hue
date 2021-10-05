8fb1b1e2df831e73274b37b52287859544f3870b
Auric Vente <mprops@protonmail.com>
2021-10-04 22:03:43 -0600

    Dropzone ctrl v fix

--- public/static/js/main/input.js
+++ public/static/js/main/input.js
@@ -41,7 +41,7 @@ Hue.setup_input = function () {
       let item = items[index]
 
       if (item.kind === "file") {
-        Hue.dropzone_image.addFile(item.getAsFile())
+        Hue.dropzone.addFile(item.getAsFile())
         return
       }
     }
