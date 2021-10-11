e44a05e701af7a9d17721b991152576f9569c5a5
Auric Vente <mprops@protonmail.com>
2021-10-11 15:58:19 -0600

    Script mod

--- public/static/tomp3.sh
+++ public/static/tomp3.sh
@@ -9,3 +9,8 @@ for FILE in audioclip/*.flac; do
   echo -e "Processing file '$FILE'";
   ffmpeg -loglevel error -i "${FILE}" -vn -ab 256k -ar 44100 -y "${FILE%.flac}.mp3";
 done;
+
+for FILE in audioclip/*.ogg; do   
+  echo -e "Processing file '$FILE'";
+  ffmpeg -loglevel error -i "${FILE}" -vn -ab 256k -ar 44100 -y "${FILE%.ogg}.mp3";
+done;
