90041a0884a89a4f67e9865096dc045c7835e3de
Auric Vente <mprops@protonmail.com>
2021-11-30 18:19:35 -0600

    TV Image lnk mods

--- public/static/assets/js/main/image.js
+++ public/static/assets/js/main/image.js
@@ -82,9 +82,9 @@ Hue.setup_image = function (mode, odata = {}) {
 
   if (!data.message) {
     if (data.size) {
-      data.message = "Uploaded image"
+      data.message = "Uploaded Image"
     } else {
-      data.message = "Linked image"
+      data.message = `Linked Image (${new URL(data.source).hostname})`
     }
   }
 
