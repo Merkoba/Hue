968c4f37889a8b1b70a0144eded09f8408b131fd
Auric Vente <mprops@protonmail.com>
2022-02-23 03:33:17 -0600

    Exception handling

--- server/sockets/modules/message_board.js
+++ server/sockets/modules/message_board.js
@@ -27,38 +27,43 @@ module.exports = function (
     }
 
     handler.process_message_links(data.message, async function (response) {
-      data.link_title = response.title,
-      data.link_description = response.description
-      data.link_image = response.image
-      data.link_url = response.url
-
-      if (data.id) {
-        let info = await db_manager.get_room(["id", socket.hue_room_id], { message_board_posts: 1, keys: 1 })
-      
-        for (let post of info.message_board_posts) {
-          if (post.id === data.id) {
-            if (post.user_id !== socket.hue_user_id) {
-              return false
+      try {
+        data.link_title = response.title,
+        data.link_description = response.description
+        data.link_image = response.image
+        data.link_url = response.url
+  
+        if (data.id) {
+          let info = await db_manager.get_room(["id", socket.hue_room_id], { message_board_posts: 1, keys: 1 })
+        
+          for (let post of info.message_board_posts) {
+            if (post.id === data.id) {
+              if (post.user_id !== socket.hue_user_id) {
+                return false
+              }
+  
+              post.message = data.message
+              post.link_title = data.link_title
+              post.link_description = data.link_description
+              post.link_image = data.link_image
+              post.link_url = data.link_url
+  
+              db_manager.update_room(socket.hue_room_id, {
+                message_board_posts: info.message_board_posts,
+              })
+  
+              handler.room_emit(socket, "edited_message_board_post", post)
+              return
             }
-
-            post.message = data.message
-            post.link_title = data.link_title
-            post.link_description = data.link_description
-            post.link_image = data.link_image
-            post.link_url = data.link_url
-
-            db_manager.update_room(socket.hue_room_id, {
-              message_board_posts: info.message_board_posts,
-            })
-
-            handler.room_emit(socket, "edited_message_board_post", post)
-            return
           }
         }
+        
+        let item = handler.push_message_board_post(socket, data)
+        handler.room_emit(socket, "new_message_board_post", item)
+      } catch (err) {
+        logger.log_error(err)
+        return
       }
-      
-      let item = handler.push_message_board_post(socket, data)
-      handler.room_emit(socket, "new_message_board_post", item)
     })
   }
 
