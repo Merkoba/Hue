003e0e50ba5743d4528c9e6641faf136f2fa50d6
Auric Vente <mprops@protonmail.com>
2022-03-01 03:27:05 -0600

    Color mods

--- public/static/assets/js/main/textparser.js
+++ public/static/assets/js/main/textparser.js
@@ -57,7 +57,7 @@ Hue.check_arrows = function (text) {
       text = `<div class='colortext greentext'>${Hue.remove_arrows(text)}</div>`
     }
   } else if (text.includes("\n")) {
-    text = `<div class='colortext whitetext'>${text}</div>`
+    text = `<div class='contrast'>${text}</div>`
   }
 
   return text
