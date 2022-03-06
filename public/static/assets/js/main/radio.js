52923c909924d06cb399620dbcba900bfc679f68
Auric Vente <mprops@protonmail.com>
2022-03-06 01:05:30 -0600

    Radio crossfade p mod 3

--- public/static/assets/js/main/radio.js
+++ public/static/assets/js/main/radio.js
@@ -677,9 +677,6 @@ Hue.crossfade_radio = function (radio) {
       let p = max_volume * 0.01
       Hue.radio_crossfade_player_1.volume = Math.max(Hue.radio_crossfade_player_1.volume - p, 0)
       Hue.radio_crossfade_player_2.volume = Math.min(Hue.radio_crossfade_player_2.volume + p, 1)
-      console.log(p)
-      console.log(Hue.radio_crossfade_player_1.volume)
-      console.log(Hue.radio_crossfade_player_2.volume)
   
       if (Hue.radio_crossfade_player_1.volume <= 0 && 
         Hue.radio_crossfade_player_2.volume >= max_volume) {
