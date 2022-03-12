a88ae43b8ff250a4f1fb4681d3aa5db6ba9eb308
Auric Vente <mprops@protonmail.com>
2022-03-12 05:26:50 -0600

    ipmod 2

--- server/sockets/modules/security.js
+++ server/sockets/modules/security.js
@@ -8,6 +8,6 @@ module.exports = function (Hue) {
   Hue.handler.log_user_ip_address = function (socket) {
     let date = new Date().toISOString()
     let info = `date: ${date} | username: ${socket.hue_username} | user_id: ${socket.hue_user_id} | ip: ${socket.hue_ip_address}`
-    Hue.ip_logger.info(info)
+    Hue.logger.info(info)
   }
 }
