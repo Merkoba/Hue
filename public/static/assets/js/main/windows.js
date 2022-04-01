33c095e0f335a56404d41a2113a4f83560096685
Auric Vente <mprops@protonmail.com>
2022-04-01 13:27:06 -0600

    Tv preview pause fix

--- public/static/assets/js/main/windows.js
+++ public/static/assets/js/main/windows.js
@@ -360,6 +360,7 @@ Hue.start_msg = function () {
       after_close: function (instance) {
         msgvars.common.after_close(instance)
         Hue.el("#tv_upload_comment_input").value = ""
+        Hue.el("#tv_upload_comment_video_preview").pause()
         Hue.tv_upload_comment_file = false
         Hue.tv_upload_comment_type = false
       },
