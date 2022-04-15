065dcebad360051a357c4f33f41c84d4e781eb15
Auric Vente <mprops@protonmail.com>
2022-04-15 17:46:23 -0600

    Play on unlock again

--- public/static/assets/js/main/media.js
+++ public/static/assets/js/main/media.js
@@ -568,7 +568,7 @@ Hue.change_media_lock = function(args) {
   Hue.change_media_lock_text(args.type)
 
   if (!new_val) {
-    Hue.change({type: args.type, play: false})
+    Hue.change({type: args.type})
   }
 
   if (args.feedback) {
