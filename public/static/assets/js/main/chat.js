3562b030d1909217603eb52267e7a5c8e8e755fe
Auric Vente <mprops@protonmail.com>
2022-05-08 14:51:20 -0600

    goto_bottom big mod 3

--- public/static/assets/js/main/chat.js
+++ public/static/assets/js/main/chat.js
@@ -1772,12 +1772,11 @@ Hue.setup_chat = function () {
     Hue.scroll_down_2()
   })
 
-  let on_resize = function () {
+  Hue.chat_resize_observer = new ResizeObserver(function () {
     Hue.goto_bottom()
     Hue.check_scrollers()
-  }
+  })
 
-  Hue.chat_resize_observer = new ResizeObserver(on_resize)
   Hue.chat_resize_observer.observe(Hue.el("#chat_area"))
   Hue.chat_resize_observer.observe(Hue.el("#chat_area_parent"))
 
