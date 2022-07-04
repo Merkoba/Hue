cfe001302e57ce973cc4c4cd6e08f98aa6e37bcd
Auric Vente <mprops@protonmail.com>
2022-07-04 06:43:22 -0600

    Commands click mod

--- public/static/assets/js/main/commands.js
+++ public/static/assets/js/main/commands.js
@@ -796,6 +796,14 @@ Hue.show_command_book = function (filter = "") {
       Hue.template_command_book_commands({commands: commands})
 
     Hue.command_book_created = true
+
+    Hue.el("#command_book_container").addEventListener("click", function (e) {
+      if (e.target.closest(".command_book_item")) {
+        let key = e.target.closest(".command_book_item").dataset.key
+        Hue.change_input(`${Hue.config.commands_prefix}${key} `)
+        Hue.close_all_modals()
+      }
+    })    
   }
 
   Hue.msg_command_book.show(function () {
