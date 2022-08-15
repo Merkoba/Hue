c73e492085d7ddd72795136528d18ffaee26a6d5
Auric Vente <mprops@protonmail.com>
2022-08-15 03:51:16 -0600

    Mods

--- public/static/assets/js/main/input.js
+++ public/static/assets/js/main/input.js
@@ -65,8 +65,11 @@ Hue.input_has_value = function (trim) {
 
 // Clears the input
 Hue.clear_input = function () {
-  if (Hue.input_has_value()) {
+  if (Hue.input_has_value(true)) {
     Hue.last_input = Hue.get_input()
+  }
+  
+  if (Hue.input_has_value()) {
     Hue.change_input("")
   }
 }
