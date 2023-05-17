b9299ef36a6e8b0b3c8ebf7ade458ed62f90878e
Auric Vente <mprops@protonmail.com>
2023-05-17 01:32:42 -0600

    Info mod

---
+++ info/pm2_hue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: "hue_main",
+    script: "hue/server/www",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    env: {
+      NODE_ENV: "production"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }],
+};
