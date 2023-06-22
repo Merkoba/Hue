1fae00990781e7e5e5608c2962898099a2116d0a
Auric Vente <mprops@protonmail.com>
2023-06-22 08:24:25 -0600

    Debouncer mods. Local storage mods

--- public/static/assets/js/main/local_storage.js
+++ public/static/assets/js/main/local_storage.js
@@ -34,9 +34,7 @@ App.save_local_storage = (ls_name, obj, force = false) => {
 App.do_save_local_storage = () => {
   for (let ls_name in App.local_storage_to_save) {
     let obj = App.local_storage_to_save[ls_name]
-
     obj = JSON.stringify(obj)
-
     localStorage.setItem(ls_name, obj)
   }
 
