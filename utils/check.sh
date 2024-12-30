6d34f0ff9e7ba5d8a5ab62822416cd1d0ae69d54
Auric Vente <mprops@protonmail.com>
2024-12-30 14:45:20 -0600

    Eslint Fixes

--- utils/check.sh
+++ utils/check.sh
@@ -6,11 +6,7 @@ last_tag=$(git describe --tags --abbrev=0)
 changed_files=$(git diff --name-only $last_tag HEAD -- '*.js')
 
 if [ -n "$changed_files" ]; then
-  npx eslint --cache $changed_files
+  npm run --silent lint $changed_files
 else
   echo "No files have changed since the last tag."
 fi
-
-# npx eslint --cache public/static/assets/js/main/*.js
-# npx eslint --cache public/static/assets/js/libs/utilz.js
-# npx eslint --cache server/sockets/modules/*.js
--- utils/fix.sh
+++ utils/fix.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-npx eslint --fix -c eslint.config.mjs public/static/assets/js/main/*.js
-npx eslint --fix -c eslint.config.mjs public/static/assets/js/libs/utilz.js
-npx eslint --fix -c eslint.config.mjs server/sockets/modules/*.js
+npm run --silent lint public/static/assets/js/main/*.js
+npm run --silent lint public/static/assets/js/libs/utilz.js
+npm run --silent lint server/sockets/modules/*.js
