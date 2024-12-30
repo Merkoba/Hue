da10678c651b6634f61817f594017d0c420e05c0
Auric Vente <mprops@protonmail.com>
2024-12-30 13:35:13 -0600

    Eslint Fixes

--- utils/check.sh
+++ utils/check.sh
@@ -2,6 +2,6 @@
 
 export NODE_OPTIONS="--no-warnings"
 
-# npx eslint --cache public/static/assets/js/main/*.js
-# npx eslint --cache public/static/assets/js/libs/utilz.js
+npx eslint --cache public/static/assets/js/main/*.js
+npx eslint --cache public/static/assets/js/libs/utilz.js
 npx eslint --cache server/sockets/modules/*.js
