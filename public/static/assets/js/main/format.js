6640aa9d30bc192701110000cdec8b8473dd3a3f
Auric Vente <mprops@protonmail.com>
2025-01-19 20:50:33 -0600

    Format fixes

--- public/static/assets/js/main/format.js
+++ public/static/assets/js/main/format.js
@@ -109,16 +109,16 @@ App.check_arrowtext = (text) => {
   return new_lines.join(`\n`)
 }
 
-App.char_regex_1 = (char, num = 1) => {
+App.char_regex_1 = (char, n = 1) => {
   let c = App.utilz.escape_regex(char)
-  let exp = `${c}{${num}}(\\S.*?\\S)${c}{${num}}`
+  let exp = `${c}{${n}}(\\S.*?\\S)${c}{${n}}`
   let regex = new RegExp(exp)
   return new RegExp(regex, `g`)
 }
 
-App.char_regex_2 = (char, num = 1) => {
+App.char_regex_2 = (char, n = 1) => {
   let c = App.utilz.escape_regex(char)
-  let exp = `\\b${c}{${num}}([^${c}]+)${c}{${num}}\\b`
+  let exp = `\\b${c}{${n}}([^${c}]+)${c}{${n}}\\b`
   let regex = new RegExp(exp)
   return new RegExp(regex, `g`)
 }
