948e282dfbb48dedb339bb5a76b888ce10a3b3f0
Auric Vente <mprops@protonmail.com>
2025-01-21 22:56:46 -0600

    Bundle mod

--- utils/bundle.js
+++ utils/bundle.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 const fs = require(`fs`)
 
-function clean(content) {
-  content = content.replace(/^\s*\/\/.*$/gm, ``)
-  return content.replace(/\n\s*\n/g, `\n\n`).trim()
-}
-
 function bundle(ext, what, manual = []) {
   let dir_path = `public/static/assets/${ext}/${what}/`
   let dir_path_target = `public/static/assets/${ext}/build/`
@@ -13,20 +8,23 @@ function bundle(ext, what, manual = []) {
   let files = fs.readdirSync(dir_path)
 
   for (let file of manual) {
-    bundle += `\n`
+    bundle += `\n\n`
     bundle += fs.readFileSync(`${dir_path}${file}`, `utf8`)
   }
+
+  bundle = bundle.trim()
 
   for (let file of files) {
     if (manual.includes(file) || !file.endsWith(`.${ext}`) || file.includes(`.bundle.`)) {
       continue
     }
 
-    bundle += `\n`
+    bundle += `\n\n`
     bundle += fs.readFileSync(`${dir_path}${file}`, `utf8`)
   }
 
-  fs.writeFileSync(`${dir_path_target}${what}.bundle.${ext}`, clean(bundle), `utf8`)
+  bundle = bundle.trim()
+  fs.writeFileSync(`${dir_path_target}${what}.bundle.${ext}`, bundle, `utf8`)
 }
 
 bundle(`js`, `main`, [`init.js`])
