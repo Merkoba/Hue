be78d651192137692160e7f01dbbb2cda5c9e736
Auric Vente <mprops@protonmail.com>
2026-01-08 09:34:27 -0600

    Mods

--- utils/release.rb
+++ utils/release.rb
@@ -4,7 +4,13 @@ require "git"
 # 1. Get Latest Tag
 repo = Git.open(".")
 
-tags = repo.tags.map(&:name).sort_by { |t| Gem::Version.new(t.sub(/^v/, "")) rescue t }
+tags = repo.tags.map(&:name).sort_by do |t|
+  begin
+    Gem::Version.new(t.sub(/^v/, ""))
+  rescue
+    Gem::Version.new("0.0.0")
+  end
+end
 
 if tags.empty?
   abort "Error: No tags found in repository. Create a tag first."
